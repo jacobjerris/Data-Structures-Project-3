66c22f8c027629f0f2aae846964952c79b860096
Jacob Jerris <60204994+jacobjerris@users.noreply.github.com>
2020-10-12 23:27:56 -0400

    Add files via upload

---
+++ project3/src/Queue.java
@@ -0,0 +1,4 @@
+public class Queue {
+//Use this to create a double-ended doubly linked list
+
+}
---
+++ project3/src/Stack.java
@@ -0,0 +1,4 @@
+public class Stack {
+    //Use this to create stack that uses a double-ended singly linked list
+
+}
